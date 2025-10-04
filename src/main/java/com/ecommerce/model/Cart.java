cd43097568ef8dc1cce82f5e36523a730b360b10
Shirley Tan Jing Yi <e0235128@u.nus.edu>
2025-10-04 16:32:40 +0800

    Cart.java

---
+++ src/main/java/com/ecommerce/model/Cart.java
@@ -0,0 +1,52 @@
+package sg.edu.nus.caproject.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Cart {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	private Customer customer;
+	
+	@OneToMany (mappedBy = "cart")
+	private List <CartItem> cartItems = new ArrayList<>();
+	
+	public Cart() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<CartItem> getCartItems() {
+		return cartItems;
+	}
+
+	public void setCartItems(List<CartItem> cartItems) {
+		this.cartItems = cartItems;
+	}
+	
+}
