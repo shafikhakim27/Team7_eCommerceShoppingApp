80efdf0ced1dc51f6ba9d1c95bf4bda56ece01f2
Shafik Hakim Bin Abdul Wahid <E1582515@u.nus.edu>
2025-10-06 08:41:49 +0800

    removed unused import

--- src/main/java/com/ecommerce/controller/AuthController.java
+++ src/main/java/com/ecommerce/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpServletRequest;
 
 
