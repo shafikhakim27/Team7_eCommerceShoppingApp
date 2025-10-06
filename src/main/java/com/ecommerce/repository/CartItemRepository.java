8fb263310ae659e1abe821b4dffd48e0e35fe92c
Shirley Tan Jing Yi <e0235128@u.nus.edu>
2025-10-06 22:45:19 +0800

    CartItemRepository.java

---
+++ src/main/java/com/ecommerce/repository/CartItemRepository.java
@@ -0,0 +1,21 @@
+//package name here
+
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sg.edu.nus.cart.model.CartItem;
+
+//Goh Ching Tard
+public interface CartItemRepository extends JpaRepository<CartItem, Long> { 
+
+	@Query("Select c from CartItem c where c.cart.customer.id = :customerId") 
+	public ArrayList<CartItem> ShowItemsInCart(@Param("customerId") Long customerId); 
+
+	@Query("Select c from CartItem c where c.product.id = :productId and c.cart.id = :cartId") 
+	public CartItem checkItemInCart(@Param("productId") Long productId, 
+			@Param("cartId") Long cartId); 
+}
