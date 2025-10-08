c8399143ccd8aa7a5557b4832c8abe769f8a69b9
Shirley Tan Jing Yi <e0235128@u.nus.edu>
2025-10-08 18:46:21 +0800

    george's and shirley's codes

--- src/main/java/com/ecommerce/service/CartInterface.java
+++ src/main/java/com/ecommerce/service/CartInterface.java
@@ -7,5 +7,9 @@ public interface CartInterface {
 	public Cart findCart(Long customerId);
 	public boolean updateCart(Cart cart);
 	public void clearCart();
+
+	//below are shirley's codes, had some conflicts with george's one so had to change
+	public void clearCart(Long customerId);
 }
+
 
