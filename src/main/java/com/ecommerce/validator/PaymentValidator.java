8d10ef6f4e2a12d765cb4eb21817d441caa3af2c
Shirley Tan Jing Yi <e0235128@u.nus.edu>
2025-10-08 19:06:24 +0800

    Create PaymentValidator.java

---
+++ src/main/java/com/ecommerce/validator/PaymentValidator.java
@@ -0,0 +1,29 @@
+//package name here
+
+import java.time.YearMonth;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import sg.edu.nus.caproject.model.Payment;
+
+@Component
+public class PaymentValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Payment.class.isAssignableFrom(clazz);
+	}
+	
+	@Override
+	public void validate(Object obj, Errors errors) {
+		Payment payment = (Payment) obj;
+		YearMonth currentYearMonth = YearMonth.now();
+		if ((payment.getExpiryDate() != null) && (payment.getExpiryDate().compareTo(currentYearMonth)<0)){
+			errors.rejectValue("expiryDate", "error.expired", "Card is expired.");
+			//this means field expiryDate has an issue, tag it with error code "error.expired" and show message "Card is expired."
+		}
+	} 
+}
+
