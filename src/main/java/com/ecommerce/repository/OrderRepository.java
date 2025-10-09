dcc4d640950b1382211ee60c57493c06a29c0d4f
CHEN HAOTING <e1546898@u.nus.edu>
2025-10-09 11:59:20 +0800

    Update OrderRepository.java

--- src/main/java/com/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/ecommerce/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 // package name here
 package com.ecommerce.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // import sg.edu.nus.caproject.model.Order;
 import com.ecommerce.model.Order;
 
 public interface OrderRepository extends JpaRepository <Order, Long> {
+  //auto-generate SQL, return customerID匹配的所有Order
+	List<Order> findByCustomerId(Long customerId);
 
 }
