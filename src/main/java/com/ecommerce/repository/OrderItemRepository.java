f13a0613bf633f9c1d69b7a67ee0806c051143f6
CHEN HAOTING <e1546898@u.nus.edu>
2025-10-09 11:57:47 +0800

    Create OrderItemRepository.java

---
+++ src/main/java/com/ecommerce/repository/OrderItemRepository.java
@@ -0,0 +1,22 @@
+package com.ecommerce.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.model.OrderItem;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	//search by name 
+	@Query("Select p from OrderItem as p where p.name like CONCAT('%',:k,'%') ") 
+	public ArrayList<OrderItem> SearchOrderItemByName(@Param("k") String keyword);
+
+	public List<OrderItem> findByOrderID(Long id);
+	 
+
+}
