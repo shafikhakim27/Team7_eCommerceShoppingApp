accd77a4160a52c8542739e8e8d6960632673a8e
CHEN HAOTING <e1546898@u.nus.edu>
2025-10-13 13:27:42 +0800

    Update OrderItemRepository.java

--- src/main/java/com/ecommerce/repository/OrderItemRepository.java
+++ src/main/java/com/ecommerce/repository/OrderItemRepository.java
@@ -12,9 +12,9 @@ import com.ecommerce.model.OrderItem;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-	//search by name
+	//search by product name
 	@Query("Select p from OrderItem as p where p.name like CONCAT('%',:k,'%') ")
 	public ArrayList<OrderItem> SearchOrderItemByName(@Param("k") String keyword);
 
-	public List<OrderItem> findByOrderID(Long id);
+
 }
